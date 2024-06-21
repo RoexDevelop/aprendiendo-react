74d007548228129103724f7d07ba9a0fe95a66e5
RoexDevelop <roexcentral@gmail.com>
2024-06-21 20:49:16 +0200

    Guardo mientras estaba haciendo la aplicacion

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,14 @@
+export function App () {
+    return(
+        <article>
+            <header>
+                <img src="https://unavatar.io/midudev" />
+            
+            <div>
+                <strong>Miguel Ángel Durán</strong>
+                <span>@midudev</span> 
+            </div>
+            </header>
+        </article>
+    )
+}
