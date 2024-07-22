767eda2e7ce7c896e38e12a88d1fbc1c5c197b13
RoexDevelop <roexcentral@gmail.com>
2024-07-22 19:36:49 +0200

    Guardado anterior

--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,11 +1,14 @@
-export function TwitterFollowCard( {userName, name, isFollowing} ){
+export function TwitterFollowCard( {children, userName = 'unknown', isFollowing} ){
   const imageSrc = `https://unavatar.io/${userName}`;
   console.log(isFollowing)
   console.log("Hola")
   
  
     const addAt = (userName) => `@${userName}`
-
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing
+        ? 'tw-followCard-button is-following'
+        : 'tw-followCard-button'
     return (
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
@@ -14,13 +17,13 @@ export function TwitterFollowCard( {userName, name, isFollowing} ){
                 src= {imageSrc}
                 alt="El avatar de mirudev" />
             <div className='tw-followCard-info'>
-                <strong>{name}</strong>
+                <strong>{children}</strong>
                 <span className='tw-followCard-infoUserName'>@{userName}</span> 
             </div>
             </header>
             <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
+                <button className= {buttonClassName}>
+                    {text}
                 </button>
             </aside>
         </article>
