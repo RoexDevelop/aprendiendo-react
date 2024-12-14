80d8972f3b69b4c3f40b09e64b46bd15ce2a6189
RoexDevelop <roexcentral@gmail.com>
2024-12-14 19:16:43 +0100

    Proyecto Local Storage

--- projects/MiProyecto_Aprendizaje/src/App.jsx
+++ projects/MiProyecto_Aprendizaje/src/App.jsx
@@ -2,32 +2,38 @@ import './App.css'
 import { useState } from 'react'
 
 export function App(){
-    localStorage.setItem("cat","andrea")
-    localStorage.setItem("dog","carlos")
+    const [inputName, setInputName] = useState('')
+    const keyName = 'name'
+    const nameLocalStorage = localStorage.getItem(keyName)
 
-    let [name, setName] = useState('')
+    
+
     return(
         <>
             <h1>Local Storage</h1>
             <h3>Introduce tu nombre</h3>
             <input
-                onChange={e => setName(e.target.value)}
+                onChange={e => setInputName(e.target.value)}
                 placeholder='Name'
             />
-            <button onClick={SaveLocalStorage}>
+            <button onClick={() => SaveNameLocalStorage(keyName, inputName)}>
                 Añadir
             </button>
-            <button>
+            <button onClick={() => DeleteNameLocalStorage("name")}>
                 Eliminar
             </button>
             <br />
-            <h2>Hola, {name}</h2>
+            <h2>Hola, {(nameLocalStorage === null)? inputName: nameLocalStorage}</h2>
         </>
     )
 }
 
-function SaveLocalStorage(name){
-    console.log(name)
-    localStorage.setItem("name",{name})
+//COMPONENTE QUE AÑADE UN NOMBRE A LOCAL STORAGE
+function SaveNameLocalStorage(keyName, value){
+    (!value)?alert("No hay nada escrito"):(localStorage.setItem(keyName, value), alert("Se ha añadido a local storage"))
+}
 
+//COMPONENTE QUE ELIMINA UN NOMBRE DE LOCAL STORAGE
+function DeleteNameLocalStorage(keyName){
+    (localStorage.getItem(keyName) === null)?alert("No hay nada en local storage"):(localStorage.removeItem(keyName), alert("Se ha eliminado de local storage"))
 }
