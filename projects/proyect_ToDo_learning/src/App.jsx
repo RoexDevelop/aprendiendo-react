229a612a61134bb8c9e9436b041d200e920b813d
RoexDevelop <roexcentral@gmail.com>
2025-02-15 20:51:04 +0100

    Actualizacion

--- projects/proyect_ToDo_learning/src/App.jsx
+++ projects/proyect_ToDo_learning/src/App.jsx
@@ -7,9 +7,17 @@ import { FaRegTrashAlt } from 'react-icons/fa';
 let nextId = 0
 export function App() {
 
+    const [isEditingCard, setIsEditingCard] = useState(false)
 
     const [inputName, setInputName] = useState()
     const [listThings, setListThings] = useState([])
+    const [inputEditName, setinputEditName] = useState()
+
+    useEffect(() => {
+        setIsEditingCard(true)
+    }, [listThings])
+
+
 
     {/** Función que añade una tarea en la lista de tareas */ }
     function addThingToList() {
@@ -17,44 +25,86 @@ export function App() {
         if (inputName) {
             setListThings([
                 ...listThings, // --> OPERADOR DE PROPAGACIÓN, añade al final de la lista un elemento
-                { id: nextId++, name: inputName }
+                { id: nextId++, name: inputName, editingCard: false}
             ])
-            setInputName("")
         } else {
             alert("no hay nada")
         }
     }
 
-    {/**Elimina un elemento de la lista */}
+    {/**Elimina un elemento de la lista */ }
     function deleteThingFromList(idBtn) {
         setListThings(listThings.filter(a => {
-            console.log(a)
             return a.id !== idBtn
         }
         ))
-        
+
     }
 
     function showList() {
-        return(<div >
-            {listThings.map(thing => (
-                <div className="card">
+        listThings.map(thing => 
+            
+            {thing.editingCard ? 
+                (<div className="card">
                     <p id={thing.id}>{thing.name}</p>
-                    <button 
+                    <button
                         id={thing.id}
                         onClick={() => deleteThingFromList(thing.id)}>
-                            Eliminar
+                        Eliminar
                     </button>
-                    <button id={thing.id}>Editar</button>
+                    <button id={thing.id} onClick={() => setIsEditingCard(true)}>Editar</button>
+                </div>) :
+                (<div className="card">
+                    <p id={thing.id}>{thing.name}</p>
+                    <button
+                        id={thing.id}
+                        onClick={() => deleteThingFromList(thing.id)}>
+                        Eliminar
+                    </button>
+                    <button id={thing.id} onClick={() => setIsEditingCard(true)}>Editar</button>
+                </div>)}
+
+            
+        )
+    }
+
+    {/**function showList() {
+        
+        return (
+            <div >
+                {listThings.map(thing => (
+                    
+                    <div className="card">
+                        <p id={thing.id}>{thing.name}</p>
+                        <button
+                            id={thing.id}
+                            onClick={() => deleteThingFromList(thing.id)}>
+                            Eliminar
+                        </button>
+                        <button id={thing.id} onClick={() => setIsEditingCard(true)}>Editar</button>
+                    </div>
+                ))}
+            </div>)
+    }**/}
+
+    function showListEdit() {
+        return (<div >
+            {listThings.map(thing => (
+                <div className="card">
+                    <input
+                        id={thing.id}
+                        onChange={e => setInputName(e.target.value)}
+                        placeholder={thing.name}
+                    />
+                    <button id={thing.id}>
+                        Guardar
+                    </button>
+                    <button id={thing.id} onClick={() => setIsEditingCard(false)}>Cancelar</button>
                 </div>
-                
             ))}
         </div>)
     }
 
-    function showListEdit(){
-
-    }
 
     return (
         <>
@@ -65,10 +115,37 @@ export function App() {
                 onChange={e => setInputName(e.target.value)}
             />
             <button onClick={addThingToList}>Añadir</button>
-
+            
             {/** Actualiza la lista para mostrarla dinámicamente */}
-            {showList()}
+            <br />
+            {console.log(listThings)}
+            {/**isEditingCard ? showListEdit() : showList()**/}
+            {listThings.map(thing => 
+            
+            thing.editingCard ? 
+                (<div className="card">
+                    <p id={thing.id}>{thing.name}</p>
+                    <button
+                        id={thing.id}
+                        onClick={() => deleteThingFromList(thing.id)}>
+                        Eliminar
+                    </button>
+                    <button id={thing.id} onClick={() => setIsEditingCard(true)}>Editar</button>
+                </div>) :
+                (<div className="card">
+                    <p id={thing.id}>{thing.name}</p>
+                    <button
+                        id={thing.id}
+                        onClick={() => deleteThingFromList(thing.id)}>
+                        Eliminar
+                    </button>
+                    <button id={thing.id} onClick={() => setIsEditingCard(true)}>Editar</button>
+                </div>)
+                
 
+            
+        )}
+            
         </>
     )
 }
