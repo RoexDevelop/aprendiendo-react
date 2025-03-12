b9703ee3f0c7c1fa94aaf437072b797fd3061abd
RoexDevelop <roexcentral@gmail.com>
2025-03-12 20:34:01 +0100

    Proyecto Películas con TailwindCss (v2)

--- projects/proyecto_tailwind-react/src/App.jsx
+++ projects/proyecto_tailwind-react/src/App.jsx
@@ -1,9 +1,47 @@
-
+import "./App.css"
 
 export function App(){
     return (
-        <>
-            <h1 class="bg-black underline">Proyectyo Tailwinddd</h1>
-        </>
+        
+        <div className="text-white bg-gray-800 min-h-screen">
+
+            {/**Barra de navegación */}
+            <nav className="flex justify-center">
+                <div className="flex w-300 justify-between">
+                    
+                    <ul className="flex ">
+                        <a href=""><li className="w-10">LOGO</li></a>
+                        <a href=""><li className="w-12 ml-2">Inicio</li></a>
+                        <a href=""><li className="w-18">Películas</li></a>
+                        <a href=""><li className="w-13">Series</li></a>
+                        <a href=""><li className="w-14">Género</li></a>
+                        
+                        
+                        
+                    </ul>
+                    <ul>
+                        <li>
+                            <input className="bg-amber-50 rounded" type="text" />
+                            <button className="text-black bg-amber-50 rounded-4xl px-4">Login</button>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            {/**Sección de películas  */}
+            <main>
+
+            </main>
+
+            {/**ASIDE */}
+            <aside>
+
+            </aside>
+
+            {/**Footer */}
+            <footer> 
+
+            </footer>
+        </div>
     )
 }
